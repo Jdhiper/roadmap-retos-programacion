329f33f2f0e7148ec2c0e45f080ececec8baa891
Jdhiper <jdhiperactivo11@gmail.com>
2026-01-15 21:53:32 -0500

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Jdhiper.py
@@ -0,0 +1,28 @@
+# El lenguaje que usare es python https://www.python.org/
+
+""" Este es un comentario de varias líneas en Python.
+    Este comentario puede tener varias lineas dentro
+    del lenguaje se usa para documentar el código.
+"""
+
+''' También se pueden usar comillas simples
+    para hacer comentarios de varias líneas.
+    Sin embargo, es más común usar comillas dobles.
+'''
+
+# Este es un comentario de una sola línea en Python. 
+
+# A continuacion la creacion de constantes en Python
+
+E = 2.71828 # Las constantes se definen igual que las variables pero en mayusculas
+a = 35      # Esta puede ser una variable representando el valor de a
+
+# Estos son los distintos tipos de datos primitivos
+
+Char = "a"
+String = "hello"
+Int = 10
+Float = 10.1232
+Boolean = True # or false
+
+print ("¡Hola, python!")
