6f6618e087ff248bb2916a40504bd910fbbe4e63
Jdhiper <jdhiperactivo11@gmail.com>
2026-01-20 16:49:53 -0500

    #01 - Python

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/Jdhiper.py
@@ -0,0 +1,96 @@
+"""* EJERCICIO:
+    
+    - Crea ejemplos utilizando todos los tipos de operadores de tu lenguaje:
+    Aritméticos, lógicos, de comparación, asignación, identidad, pertenencia, bits...
+    (Ten en cuenta que cada lenguaje puede poseer unos diferentes)
+    
+    Utilizando las operaciones con operadores que tú quieras, crea ejemplos
+    que representen todos los tipos de estructuras de control que existan
+    en tu lenguaje:
+    
+    Condicionales, iterativas, excepciones...
+    - Debes hacer print por consola del resultado de todos los ejemplos.
+"""
+#   Acontinuacion se observaran los distintos tipos de operadores dentro de python
+
+# Aritmeticos
+
+#   SUMA
+suma = 5 + 4
+print(suma)
+#   RESTA 
+resta = 5 - 4
+print(resta)
+#   Multiplicacion
+mult = 8 * 4
+print(mult)
+#   Division
+Div = 10/3
+print(Div)
+#   Modulo
+Mod = 65%2
+print(Mod)
+#   Exponenciacion
+Exp = 10**3
+print(Exp)
+#   Division entera
+DivE = 25//3
+print(DivE)
+
+#COMPARACION
+
+    # Igual a.
+print(f" 1 == 1 es igual a {1 == 1}")
+    # Diferente de.
+print(f" 1 != 2 es igual a {1 != 2}")
+    # Mayor que.
+print(f" 12 > 1 es igual a {2 > 1}")
+    # Menor que.
+print(f" 4 < 10 es igual a {4 < 10}")
+    # Mayor o igual que.
+print(f" 3 >= 3 es igual a {3 >= 3}")
+    # Menor o igual que.
+print(f" 2 <= 2 es igual a {2 <= 2}")
+
+# Operadores logicos
+
+varT = True
+varF = False
+
+print(f"{varF and varT}") # en and solo devuelve true cuando ambas variables son true
+print(f"{varF or varT}") # en or devuelve true cuando al menos una variable es true
+print(f"{not varT} y {not varF}") # not invierte el booleano
+
+# Asignacion
+x = 10
+print(x)
+x += 5
+print(x)
+x -= 5
+print(x)
+x *= 5
+print(x)
+x /= 5
+print(x)
+x %= 5
+print(x)
+x //= 5
+print(x)
+x **= 5
+print(x)
+x &= 5
+print(x)
+x |= 5
+print(x)
+x &= 5
+print(x)
+x ^= 5
+print(x)
+x >>= 5
+print(x)
+x &lt&lt= 5
+print(x)
+
+# Identidad
+
+# Pertenencia
